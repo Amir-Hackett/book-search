27ac039c62948f0977a3a3a7c6cd47b04b92dad7
Amir <amir.hackett@gmail.com>
2022-07-15 23:31:07 -0400

    heroku fix

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+ 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
